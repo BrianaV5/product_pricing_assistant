26a27785135fd5c17a7b7f563f676dacd28f7e6a
Briana Viscount <Brianaviscount@usf.edu>
2026-02-01 17:07:47 -0500

    first commit

---
+++ pricing_assistant.js
@@ -0,0 +1,22 @@
+ //create and Intialize Variables
+let productName = "Hairbrush" //string
+let costPerUnit = 11.50;
+let basePrice = 18.99;
+let discountRate = 0.10;
+const salesTaxRate = 0.08;// tax rate 
+let fixedMonthlyCosts = 1300;
+
+// Calculate Pricing & Profit Metrics 
+let discountedPrice = basePrice * (1 - discountRate);
+let finalPriceWithTax = discountedPrice * (1 + salesTaxRate);
+let profitPerUnit = finalPriceWithTax - costPerUnit;
+let breakEvenUnits = Math.ceil(fixedMonthlyCosts / profitPerUnit);
+let isProfitablePerUnit = profitPerUnit > 0; // boolean
+
+//print to console
+console.log(`Product name: ${productName}`);
+console.log(`Discounted price: $${discountedPrice.toFixed(2)}`);
+console.log(`Final price with tax: $${finalPriceWithTax.toFixed(2)}`);
+console.log(`Profit per unit: $${profitPerUnit.toFixed(2)}`);
+console.log(`Break-even units: ${breakEvenUnits}`);
+console.log(`Per-unit profitability ${isProfitablePerUnit}`);
